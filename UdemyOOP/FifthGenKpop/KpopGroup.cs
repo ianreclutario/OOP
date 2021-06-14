2e987100b3a9dcae5ece4f3b8a86cd36ff8ca765
ianre <ianre@DESKTOP-V5SA1I6>
2021-06-15 01:10:01 +0800

    first commit

---
+++ UdemyOOP/FifthGenKpop/KpopGroup.cs
@@ -0,0 +1,43 @@
+﻿namespace FifthGenKpop
+{
+    public class KpopGroup
+    {
+
+        public string visual { get; set; }
+        
+        public string mainVocalist { get; set; }
+        public string leadVocalist { get; set; }
+        public string mainDancer { get; set; }
+        public string leadDancer { get; set; }
+        public string rapper { get; set; }
+
+        public KpopGroup()
+        {
+            
+        }
+        
+        public KpopGroup(KpopGroup k)
+        {
+            visual = k.visual;
+            mainVocalist = k.mainVocalist;
+            leadVocalist = k.leadVocalist;
+            mainDancer = k.mainDancer;
+            leadDancer = k.leadDancer;
+            rapper = k.rapper;
+        }
+        public KpopGroup(string _visual, string _mainVocalist, string _leadVocalist, string _mainDancer, string _leadDancer, string _rapper)
+        {
+            visual = _visual;
+            mainVocalist = _mainVocalist;
+            leadVocalist = _leadVocalist;
+            mainDancer = _mainDancer;
+            leadDancer = _leadDancer;
+            rapper = _rapper;
+        }
+        public KpopGroup(string mainDancer, string leadDancer)
+        {
+            this.mainDancer = mainDancer;
+            this.leadDancer = leadDancer;
+        }
+    }
+}
--- UdemyOOP/Program.cs
+++ UdemyOOP/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using FifthGenKpop;
 namespace UdemyOOP
 {
     class Program
     {
+   
+
         static void Main(string[] args)
         {
+            KpopGroup lpnr = new KpopGroup("Hirai Momo","Park Jihyo");
+
+            lpnr.mainVocalist = "Park Jihyo";
+            lpnr.leadVocalist = "Im Nayeon";
+
+
+
+            lpnr.visual = "Shin Ryujin";
+            lpnr.mainVocalist = "Ian Rex Clutario";
+            lpnr.leadVocalist = "Myoui Mina";
+            lpnr.mainDancer = "Hirai Momo";
+            lpnr.leadDancer = "Park Jihyo";
+            lpnr.rapper = "Quejano Charles";
         }
     }
 }
